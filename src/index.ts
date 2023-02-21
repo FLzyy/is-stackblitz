a65aa4825f56d72afab911c43325725ba1310598
FLzyy <flzyy72@gmail.com>
2023-02-21 08:48:42 +0800

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
 export default function isStackBlitz(): boolean {
   return (
-    process.env.GIT_PROXY?.includes("stackblitz") ||
-    process.env.SHELL?.includes("jsh") ||
+    process.env.GIT_PROXY?.includes("stackblitz") ??
+    process.env.SHELL?.includes("jsh") ??
     "STACKBLITZ" in process.env
   );
 }
