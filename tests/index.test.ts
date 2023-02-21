281efa2a1fa104b7e6b6a4cff1402df31f8f71f1
FLzyy <flzyy72@gmail.com>
2023-02-21 08:39:20 +0800

    write tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,9 +1,29 @@
-/**
- * Testing:
- *
- * This file is what is used to test `index.ts` using the built in node:test module.
- *
- * Delete if requirements are unclear or frequently changes.
- */
-
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+import { equal } from "node:assert/strict";
 import { describe, it } from "node:test";
+import isStackblitz from "../src/index";
+
+describe("not replit", () => {
+  it("should return false", () => {
+    equal(isStackblitz(), false);
+  });
+});
+
+describe("in replit", () => {
+  it("should return true (STACKBLITZ)", () => {
+    process.env.STACKBLITZ = "1";
+    equal(isStackblitz(), true);
+    delete process.env.STACKBLITZ;
+  });
+
+  it("should return true (GIT_PROXY = 'https://p.stackblitz.com')", () => {
+    const initial = process.env.GIT_PROXY;
+    process.env.GIT_PROXY = "https://p.stackblitz.com";
+    equal(isStackblitz(), true);
+    if (initial) {
+      process.env.GIT_PROXY = initial;
+    } else {
+      delete process.env.GIT_PROXY;
+    }
+  });
+});
